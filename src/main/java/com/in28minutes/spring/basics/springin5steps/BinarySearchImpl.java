6421e647ac702f583f7533c88428ba7f8147a10c
Kaiyi <neovincent@163.com>
2020-07-12 22:13:48 -0700

    Annotation

--- src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,6 +1,7 @@
 package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,7 @@ public class BinarySearchImpl {
 
     // BinarySearchImpl dependent on sortAlgorithm
     @Autowired
+    @Qualifier("quick")
     private SortAlgorithm sortAlgorithm;
 
     public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
@@ -21,3 +23,4 @@ public class BinarySearchImpl {
         return 3;
     }
 }
+
