8b0d9e2729dc52b7f67c44938755520594cbf813
Kaiyi <neovincent@163.com>
2020-08-19 00:16:04 -0700

    init unit test

---
+++ junit-in-5-steps/test/com/in28minutes/junit/MyMathTest.java
@@ -0,0 +1,12 @@
+package com.in28minutes.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyMathTest {
+
+    @Test
+    void sum() {
+    }
+}
